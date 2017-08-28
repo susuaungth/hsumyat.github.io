ec07be513871c65b9b569f8c49d55c3e73d30caf
hsumyatnoenwe <hsumyatnoenwe@Gmail.com>
2017-08-28 23:11:26 +0630

    General fix all

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
 
 $(document).ready(function(){
-	$("#header").hover(function(){
-		$('.blocks.first-intro-info .icon-html').toggleClass('ht-hover-icon');
-	});
-	$("#header").hover(function(){
-		$('.blocks.first-intro-info .icon-code').toggleClass('code-hover-icon');
-	});
-	$("#footer").hover(function(){
-		$('.blocks.last-intro-info .icon-html').toggleClass('ht-hover-icon');
-	});	
-	$("#footer").hover(function(){
-		$('.blocks.last-intro-info .icon-code').toggleClass('code-hover-icon');
-	});
-
+	// $("#abtme").click(function() {
+	// 	$('html, body').animate({
+	// 	    scrollDown: $("#down").offset().down
+	// 	}, 2000);
+	// });
 });
