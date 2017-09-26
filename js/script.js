bf005bb237aa371bf3bdb62242483f695e6b7b6d
hsumyatnoenwe <hsumyatnoenwe@gmail.com>
2017-09-26 20:39:47 +0630

    Opacity arrows

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
 
 $(document).ready(function(){
-  $(".abt-link").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.("#abt-me");
-
-     $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){ 
-        
-        window.location.hash = hash;
-      });
-    } 
-  });
+   $(".abt-link").on("click" ,function(){
+     scrolled=scrolled-300;
+        $("#abtme").animate({
+          scrollTop:  scrolled
+     });
+   });
 });
+
