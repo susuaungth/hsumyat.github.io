6a63e555310ccef78880b2b89f1c090728ff6289
hsumyatnoenwe <hsumyatnoenwe@gmail.com>
2018-01-17 16:34:59 +0630

    add transition

--- js/script.js
+++ js/script.js
@@ -6,5 +6,16 @@ $(document).ready(function(){
           scrollTop:  scrolled
      });
    });
+   
+   	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
 
